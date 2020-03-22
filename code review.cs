5bc6835fe269d1bd1aab45851175bae757f86c5d
JMarrows <60883201+JMarrows@users.noreply.github.com>
2020-03-22 14:22:25 +0000

    Add files via upload

---
+++ code review.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeToBeReviewed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] git_1a = ;
+            string[] git_1a =;
+            string[] git_1a =;
+            string[] git_1a =;
+            string[] git_1a =;
+            string[] git_1a =;
+            Console.WriteLine("Whic files would you like to compare? Set 1,2 or 3");
+            string UserInput = Console.ReadLine();
+            if (UserInput == "1"){
+
+            }
+            if (UserInput == "2"){
+
+            }
+            if (UserInput == "3"){
+
+            }
+        }
+    }
+}
